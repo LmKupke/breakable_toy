ef6544272a8f9de25ed27636b977ecd8f17d19f7
Lincoln Kupke <lincolnkupke@gmail.com>
2016-07-18 17:41:53 -0400

    fixing hound violations on support

--- spec/support/koala_fake.rb
+++ spec/support/koala_fake.rb
@@ -11,13 +11,14 @@ class KoalaFake
       { "mutual_friends" =>
         { "data" =>
           [{ "name" => "Carol Alabebdiafcja Smithberg",
-            "id" => "105338816560282"
+          "id" => "105338816560282"
           },
           { "name" => "Charlie Alabeicdfjfdi Smithson",
-            "id" => "100610493702066"
+          "id" => "100610493702066"
           }],
          "paging" => { "cursors" => { "before" => "MTEzNTkzNzM1NzMyNjk1",
-           "after" => "MTEzNTkzNzM1NzMyNjk1" } },
+           "after" => "MTEzNTkzNzM1NzMyNjk1" }
+         },
          "summary" => { "total_count" => 1 } },
        "mutual_likes" => { "data" => [], "summary" => { "total_count" => 0 } },
        "id" => "dXNlcl9jb250ZAXh0OgGQDepANcR5qySRUOeivSUWTV6nSSCOPXjBLRt09rEE5zuudygAO4GTPQ8n0BuzmMAZB660pGu3YiS29a3HStXNF1lkNosDbHry57LepVH4jqrQZD" },
