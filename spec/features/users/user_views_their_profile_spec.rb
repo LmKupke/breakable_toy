eadc9f16a2bbf2be00ff21399029ef98fa1e6bb5
Lincoln Kupke <lincolnkupke@gmail.com>
2016-07-18 18:58:01 -0400

    fixing new hound error

--- spec/features/users/user_views_their_profile_spec.rb
+++ spec/features/users/user_views_their_profile_spec.rb
@@ -81,24 +81,24 @@ feature "user views profile", %{
       )
 
       past_friend_event4 = build(
-        :event,
-        organizer: friend,
-        name: "Event 4",
-        date: Time.zone.now - 4.hours
+          :event,
+          organizer: friend,
+          name: "Event 4",
+          date: Time.zone.now - 4.hours
       )
 
       past_friend_event5 = build(
-          :event,
-          organizer: friend,
-          name: "Event 5",
-          date: Time.zone.now - 5.hours
+            :event,
+            organizer: friend,
+            name: "Event 5",
+            date: Time.zone.now - 5.hours
         )
 
       past_friend_event6 = build(
-          :event,
-          organizer: friend,
-          name: "Event 6",
-          date: Time.zone.now - 6.hours
+            :event,
+            organizer: friend,
+            name: "Event 6",
+            date: Time.zone.now - 6.hours
         )
 
       past_friend_event1.save(validate: false)
