17527db78b201cd76f05d22c96b88edeeb34a916
Lincoln Kupke <lincolnkupke@gmail.com>
2016-07-18 18:04:47 -0400

    getting rid of hound on koala_fake

--- spec/support/koala_fake.rb
+++ spec/support/koala_fake.rb
@@ -1,5 +1,4 @@
 class KoalaFake
-  User = Struct.new(:name, :id)
   USERS = [
     { "name" => "Elizabeth Alabdfbjgafef Huisky", "id" => "113593735732695" },
     { "name" => "Carol Alabebdiafcja Smithberg", "id" => "105338816560282" },
@@ -11,15 +10,16 @@ class KoalaFake
       { "mutual_friends" =>
         { "data" =>
           [{ "name" => "Carol Alabebdiafcja Smithberg",
-          "id" => "105338816560282"
+            "id" => "105338816560282"
           },
           { "name" => "Charlie Alabeicdfjfdi Smithson",
-          "id" => "100610493702066"
+            "id" => "100610493702066"
           }],
-         "paging" => { "cursors" => { "before" => "MTEzNTkzNzM1NzMyNjk1",
-           "after" => "MTEzNTkzNzM1NzMyNjk1" }
-         },
-         "summary" => { "total_count" => 1 } },
+         "paging" => {
+           "cursors" =>
+           { "before" => "MTEzNTkzNzM1NzMyNjk1", "after" => "MTEzNTkzNzM1NzMyNjk1" }
+         }, "summary" => { "total_count" => 1 }
+       },
        "mutual_likes" => { "data" => [], "summary" => { "total_count" => 0 } },
        "id" => "dXNlcl9jb250ZAXh0OgGQDepANcR5qySRUOeivSUWTV6nSSCOPXjBLRt09rEE5zuudygAO4GTPQ8n0BuzmMAZB660pGu3YiS29a3HStXNF1lkNosDbHry57LepVH4jqrQZD" },
      "id" => "100610493702066" }
