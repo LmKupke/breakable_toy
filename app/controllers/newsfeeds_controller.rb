178dc1d8827351a9f47ddd48aa03d05ed62127e3
Lincoln Kupke <lincolnkupke@gmail.com>
2016-07-20 22:15:10 -0400

    styled the newsfeed and it only shows friend events, added friends profiles on profile page

--- app/controllers/newsfeeds_controller.rb
+++ app/controllers/newsfeeds_controller.rb
@@ -4,7 +4,7 @@ class NewsfeedsController < AuthenticateController
     @friends = graph.friendlist
     array = @friends.map { |user| user["id"]}
     fr = User.where(uid: array)
-    @friendevents= fr.map(&:all_events).flatten.uniq
+    @friendevents= fr.map(&:newsfeed_events).flatten.uniq
     @friendevents = @friendevents - current_user.events
     @friendevents = @friendevents.reject { |c| c.nil? }
   end
