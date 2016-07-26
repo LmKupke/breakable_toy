f0260bbd97271f6e745844c4e745c99f4dbea723
Lincoln Kupke <lincolnkupke@gmail.com>
2016-07-26 11:12:59 -0400

    got twillio to work

--- app/controllers/api/newsfeeds_controller.rb
+++ app/controllers/api/newsfeeds_controller.rb
@@ -9,13 +9,13 @@ class Api::NewsfeedsController < AuthenticateController
     if event.organizer.phonenumber == nil && current_user.phonenumber == nil
       flash[:alert] = "Your phonenumber and #{event.organizer.name}'s number is not saved!"
     elsif event.organizer.phonenumber != nil && current_user.phonenumber == nil
-      flash[:alert] =  "Your phonenumber is not saved! Please add your number so you ping the #{event.organizer.name}"
+      flash[:alert] =  "Your phonenumber is not saved! Please add your number so you can ping the #{event.organizer.name}. To add your phonenumber go to your profile!"
     elsif event.organizer.phonenumber == nil && current_user.phonenumber != nil
       flash[:alert] = "Seems like #{event.organizer.name} doesn't have their number on NOMO-FOMO. Text them and let them know to added it."
     else
       client = Twilio::REST::Client.new Rails.application.secrets.twilio_account_sid, Rails.application.secrets.twilio_auth_token
-      message = client.messages.create from: '+16172063319', to: event.organizer.phonenumber, body: "Your friend #{current_user.name} wants to join your Event, #{event.name} on NOMO-FOMO! Invite them to it!"
-      render plain: message.status
+      message = client.messages.create from: '+16172063319', to: "+1#{event.organizer.phonenumber}", body: "Your friend #{current_user.name} wants to join your Event, #{event.name} on NOMO-FOMO! Invite them to it!"
+      # render plain: message.status
     end
     redirect_to newsfeeds_path
   end
