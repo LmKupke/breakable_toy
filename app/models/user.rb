566759a2d92581fbcad4f07f23cdbeea9aaba255
Lincoln Kupke <lincolnkupke@gmail.com>
2016-07-26 18:36:44 -0400

    possible fix too

--- app/models/user.rb
+++ app/models/user.rb
@@ -32,7 +32,8 @@ class User < ActiveRecord::Base
   end
 
   def self.from_omniauth(auth)
-   where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+    puts auth
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
      user.email = auth.info.email
      user.password = Devise.friendly_token[0,20]
      user.name = auth.info.name   # assuming the user model has a name
@@ -41,7 +42,7 @@ class User < ActiveRecord::Base
      user.expires_at = Time.at(auth.credentials.expires_at)
      user.timezone = auth.extra.raw_info.timezone
      user.phonenumber = nil
-   end
+    end
   end
 
   def self.new_with_session(params, session)
